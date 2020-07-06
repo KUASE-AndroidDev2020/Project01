c279c63f5e0b535e085a55a67fb696c282c92aae
2020M017 <2020m017@kuas.ac.jp>
2020-07-06 10:32:55 +0900

    Calendar get today

--- app/src/main/java/com/example/project01/MainActivity.java
+++ app/src/main/java/com/example/project01/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.project01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final CalendarView calendarView = new CalendarView(this);
+//現在の日付を取得する
+        Calendar selectedCalendar = Calendar.getInstance();
+        long selectedTimeInMills = selectedCalendar.getTimeInMillis();
     }
 }
