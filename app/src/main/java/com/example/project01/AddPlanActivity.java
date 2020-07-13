9d0306777ee72f71c458a08717bee3239504a221
2020M017 <2020m017@kuas.ac.jp>
2020-07-13 22:35:06 +0900

    Add to get back MainActivity

--- app/src/main/java/com/example/project01/AddPlanActivity.java
+++ app/src/main/java/com/example/project01/AddPlanActivity.java
@@ -2,7 +2,9 @@ package com.example.project01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AddPlanActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class AddPlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plan);
+    }
+
+    public void goToMainActivity(View view) {
+        Intent toAdd = new Intent(this,MainActivity.class);
+        startActivity(toAdd);
     }
 }
