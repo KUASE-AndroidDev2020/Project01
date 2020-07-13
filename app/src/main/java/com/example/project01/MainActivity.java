0a779b14f8c9e8013331ebcfa4e573ae69a7cfe9
2020M017 <2020m017@kuas.ac.jp>
2020-07-14 01:00:31 +0900

    Add hint message

--- app/src/main/java/com/example/project01/MainActivity.java
+++ app/src/main/java/com/example/project01/MainActivity.java
@@ -4,19 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.service.autofill.OnClickAction;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ListView;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    ArrayAdapter<String> adapter;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final CalendarView calendarView = new CalendarView(this);
-//現在の日付を取得する
         Calendar selectedCalendar = Calendar.getInstance();
         long selectedTimeInMills = selectedCalendar.getTimeInMillis();
     }
